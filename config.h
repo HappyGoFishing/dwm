f71d08b66b0452780562928e692ca6d4ff8ea5d0
KieranCrossland <kierancrossland1st@gmail.com>
2022-12-29 21:12:52 +0000

    set incnmaster to XK_0 and XK_i because I dont need them

--- config.h
+++ config.h
@@ -1,5 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
+
+//Comments starting with "//" are Kieran's comments and aren't part of the vanilla dwm sources.
+
+
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -60,25 +64,30 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL};
+static const char *dmenu_powercmd[] = { "dmenu-power", NULL};
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_f,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = dmenu_powercmd } },
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+        //These are unwanted binds. Stopped compiler warnings by binding them to XK_i and XK_o
+	//These are never intended to be used and one day I will fully remove them from dwm.c et cetera
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = +1 } },
+        { MODKEY,                       XK_i,      incnmaster,     {.i = -1 } },
+	
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-	//{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	//{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      spawn,          {.v = browsercmd } },
-
-	//{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+      //{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+      //{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+      //{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -96,7 +105,6 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
