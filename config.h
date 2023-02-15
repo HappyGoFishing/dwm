dc5242ef0499dee86096439b7fbe02de0c4b63e8
KieranCrossland <kierancrossland1st@gmail.com>
2023-02-15 19:17:44 +0000

    Update config.h

--- config.h
+++ config.h
@@ -82,7 +82,7 @@ static const Key keys[] = {
 	
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,				XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
     { MODKEY,                       XK_y,      setlayout,      {.v = &layouts[0]} },
