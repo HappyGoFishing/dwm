74d656f76516f759dee3fa5363232161783b2218
KieranCrossland <kierancrossland1st@gmail.com>
2023-02-15 02:10:59 +0000

    Update config.h

--- config.h
+++ config.h
@@ -60,10 +60,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-T Simple Terminal", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox", NULL};
 static const char *dmenu_powercmd[] = { "dmenu-power", NULL};
-static const char *volumecmd[] = { "st -e pulsemixer", NULL};
+static const char *volumecmd[] = { "st"," -e pulsemixer" NULL};
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
